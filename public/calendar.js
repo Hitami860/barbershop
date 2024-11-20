039d2b917a339e937b61070c8e3ba734ba096eef
Hitami860 <Hitami86000@gmail.com>
2024-11-20 13:43:38 +0100

    upd

--- public/calendar.js
+++ public/calendar.js
@@ -3,15 +3,17 @@ document.addEventListener('DOMContentLoaded', function () {
     var calendarEl = document.getElementById('calendar');
     var calendar = new FullCalendar.Calendar(calendarEl, {
         initialView: 'timeGridWeek',
+        selectable: true,
         eventOverlap: false,
         allDayText: "Jour / Heure",
-        height: 500,
+        height: 800,
         slotDuration: '00:30:00',
         slotLabelInterval: '00:30',
         slotLabelFormat: {
             hour: '2-digit',
             minute: '2-digit',
             hour12: false
+            
         },
 
     });
